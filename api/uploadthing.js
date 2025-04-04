c7884b00b90498bd6b0bdb80c08fd8263430377c
copturehub <accounts@copture.com>
2025-04-04 13:05:04 +0200

    Update uploadthing.js

--- api/uploadthing.js
+++ api/uploadthing.js
@@ -1,12 +1,17 @@
-import { createUploadthing, type FileRouter } from "uploadthing/server";
+import { init, createUploadthing, type FileRouter } from "uploadthing/server";
 import { uploadthingHandler } from "uploadthing/next";
+
+// 🧠 Initiera med miljövariablerna (läggs in automatiskt från Vercel Environment Variables)
+init({
+  apiKey: process.env.UPLOADTHING_SECRET,
+});
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
   pilotUploader: f({ image: { maxFileSize: "4MB" } })
     .onUploadComplete(({ metadata, file }) => {
-      console.log("Uppladdad fil:", file);
+      console.log("✅ Uppladdad fil:", file);
     }),
 } satisfies FileRouter;
 
