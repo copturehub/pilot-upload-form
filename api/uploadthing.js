950ae2aea9ecb6da6ec3d935a643945b36a3dffd
copturehub <accounts@copture.com>
2025-04-04 11:54:03 +0200

    Create uploadthing.js

---
+++ api/uploadthing.js
@@ -0,0 +1,13 @@
+import { createUploadthing, type FileRouter } from "uploadthing/server";
+import { uploadthingHandler } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  pilotUploader: f({ image: { maxFileSize: "4MB" } })
+    .onUploadComplete(({ metadata, file }) => {
+      console.log("Uppladdad fil:", file);
+    }),
+} satisfies FileRouter;
+
+export const { GET, POST } = uploadthingHandler({ router: ourFileRouter });
