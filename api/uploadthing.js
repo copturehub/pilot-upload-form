8bd6ff03bf4835f68dae6cea41730c7ac5e9e4bd
copturehub <accounts@copture.com>
2025-04-04 14:37:38 +0200

    Update uploadthing.js

--- api/uploadthing.js
+++ api/uploadthing.js
@@ -1,16 +1,23 @@
-import { createUploadthing } from "uploadthing/server";
+import { createUploadthing, type FileRouter } from "uploadthing/server";
 import { createRouteHandler } from "uploadthing/next";
+import { z } from "zod"; // 👈 Validering krävs i v7
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
   pilotUploader: f({ image: { maxFileSize: "4MB" } })
+    .input(
+      z.object({
+        pilot: z.string().min(1),
+        project: z.string().min(1),
+      })
+    )
     .onUploadComplete(({ file, metadata }) => {
       console.log("✅ Upload complete!");
       console.log("Filnamn:", file.name);
       console.log("Metadata:", metadata);
     }),
-};
+} satisfies FileRouter;
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
