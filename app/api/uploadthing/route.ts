2083804d650da7e1203b0cf77c153e8cc06ed037
copturehub <accounts@copture.com>
2025-04-06 15:17:27 +0200

    Update route.ts

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,23 +1,8 @@
 // app/api/uploadthing/route.ts
-import { createUploadthing, type FileRouter } from "uploadthing/server";
+
 import { createRouteHandler } from "uploadthing/next";
-
-const f = createUploadthing();
-
-export const ourFileRouter = {
-  pilotUploader: f({ image: { maxFileSize: "4MB" } })
-    .onUploadComplete(({ file, metadata }) => {
-      console.log("✅ Upload complete!");
-      console.log("Filnamn:", file.name);
-      console.log("Metadata:", metadata);
-    }),
-} satisfies FileRouter;
+import { ourFileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  config: {
-    error: (err) => {
-      console.error("🔥 UploadThing Error:", err);
-    },
-  },
 });
